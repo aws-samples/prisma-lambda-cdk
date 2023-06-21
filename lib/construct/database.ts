43fa3470aba76a13a357396888264075f93ef80c
tmokmss <tomookam@live.jp>
2023-06-22 08:48:56 +0900

    use writer instead of instanceProps

--- lib/construct/database.ts
+++ lib/construct/database.ts
@@ -1,7 +1,7 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as rds from 'aws-cdk-lib/aws-rds';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
-import { Construct } from 'constructs';
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import { Construct } from "constructs";
 
 interface DatabaseProps {
   vpc: ec2.IVpc;
@@ -17,21 +17,20 @@ export class Database extends Construct {
 
     const vpc = props.vpc;
 
-    const securityGroup = new ec2.SecurityGroup(this, `SecurityGroup`, {
+    const securityGroup = new ec2.SecurityGroup(this, "SecurityGroup", {
       vpc,
     });
 
-    const cluster = new rds.DatabaseCluster(this, `Cluster`, {
-      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_3_03_0 }),
+    const cluster = new rds.DatabaseCluster(this, "Cluster", {
       // Please read README.md ### Using Postgres section if you want to use Postgres
       // engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_15_2 }),
-      instanceProps: {
-        vpc,
-        vpcSubnets: vpc.selectSubnets({ subnets: vpc.isolatedSubnets.concat(vpc.privateSubnets) }),
+      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_3_03_0 }),
+      writer: rds.ClusterInstance.provisioned("Writer", {
         instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
-        securityGroups: [securityGroup],
-      },
-      instances: 1,
+      }),
+      vpc,
+      vpcSubnets: vpc.selectSubnets({ subnets: vpc.isolatedSubnets.concat(vpc.privateSubnets) }),
+      securityGroups: [securityGroup],
       storageEncrypted: true,
     });
 
