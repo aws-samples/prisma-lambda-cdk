43ced80c98b5fdcaaaa9bce1db2624368a700450
tmokmss <tomookam@live.jp>
2024-01-13 18:34:14 +0900

    add exclude characters for password generation
    we add ,=^

--- lib/construct/database.ts
+++ lib/construct/database.ts
@@ -16,16 +16,22 @@ export class Database extends Construct {
 
     const vpc = props.vpc;
 
+    // Please read README.md ### Using Postgres section if you want to use Postgres
+    // engine = rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_15_2 }),
+    const engine = rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_3_03_0 });
     const cluster = new rds.DatabaseCluster(this, "Cluster", {
-      // Please read README.md ### Using Postgres section if you want to use Postgres
-      // engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_15_2 }),
-      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_3_03_0 }),
       writer: rds.ClusterInstance.provisioned("Writer", {
         instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
+      engine,
       }),
       vpc,
       vpcSubnets: vpc.selectSubnets({ subnets: vpc.isolatedSubnets.concat(vpc.privateSubnets) }),
       storageEncrypted: true,
+      // Exclude some more special characters from password string to avoid from URI encoding issue
+      // see: https://www.prisma.io/docs/orm/reference/connection-urls#special-characters
+      credentials: rds.Credentials.fromUsername(engine.defaultUsername ?? "admin", {
+        excludeCharacters: " %+~`#$&*()|[]{}:;<>?!'/@\"\\,=^",
+      }),
     });
 
     this.cluster = cluster;
