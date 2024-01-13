0a63adfe3cf1ecf3cde101c906c919b51dea5c41
Zach Millman <zach.millman@gmail.com>
2024-01-12 17:48:56 -0800

    Escape special characters in password for Prisma connection URL
    Just ran into this issue and it took several hours to debug.
    
    Prisma requires any special characters in the connection string to be %-encoded.
    
    If you happen to provision a DB with special characters in the password (reasonably likely if you let RDS pick the values) it will fail to connect.
    
    https://www.prisma.io/docs/orm/reference/connection-urls#special-characters

--- lib/stack/prisma-lambda-cdk-stack.ts
+++ lib/stack/prisma-lambda-cdk-stack.ts
@@ -25,7 +25,7 @@ export class PrismaLambdaCdkStack extends cdk.Stack {
         // You should create a database user with minimal privileges for your application.
         // Also refer to: https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/UsingWithRDS.MasterAccounts.html
         username: database.secret.secretValueFromJson("username").toString(),
-        password: database.secret.secretValueFromJson("password").toString(),
+        password: encodeURIComponent(database.secret.secretValueFromJson("password").toString()),
       },
     });
 
